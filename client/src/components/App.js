bc35fd519dea76881dccd8b81e49e0ff61edca6c
alireza1989 <alireza.darbehani@gmail.com>
2019-01-08 18:15:46 -0500

    setup reducers and react components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header'
+
+// dummy components 
+const DashBoard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return (
         <div>
-            Hi there!
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={DashBoard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDom from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import App from './components/App';
 
+const store = createStore(()=>[], {}, applyMiddleware());
+
 ReactDom.render(
-    <App />,
+    <Provider store={store}><App /></Provider>,
     document.querySelector('#root')
 );
