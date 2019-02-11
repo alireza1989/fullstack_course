7439fb3138dc9109c594909ab9464e73b4bd28f1
alireza1989 <alireza.darbehani@gmail.com>
2019-02-11 11:36:15 -0500

    Upadte the next step

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk'
 
 import App from './components/App';
 
-const store = createStore(()=>[], {}, applyMiddleware());
+const store = createStore(()=>[], {}, applyMiddleware(reduxThunk));
 
 ReactDom.render(
     <Provider store={store}><App /></Provider>,
